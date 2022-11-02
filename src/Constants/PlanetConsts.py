e422ace73ffc93a5cf6314e80360103b0db82d0e
Sorren Chandra <sorrenchandra375@gmail.com>
2022-11-02 15:27:49 -0700

    added Mu values for all planets in km^3/s^2

--- src/Constants/PlanetConsts.py
+++ src/Constants/PlanetConsts.py
@@ -21,12 +21,12 @@ SUN_MASS       = 1.988e+30      # [kg]
 SUN_RADIUS     = 6957000        # [km]
 
 # Mercury 
-MERCURY_MU     = 1
+MERCURY_MU     = 2.2030e+4      # [km^3 / s^2]
 MERCURY_MASS   = 0.330e+24      # [kg]
 MERCURY_RADIUS =(4879)/2        # [km]
 
 # Venus
-VENUS_MU       = 1   
+VENUS_MU       = 3.249e+5       # [km^3 / s^2]
 VENUS_MASS     = 4.87e+24       # [kg]
 VENUS_RADIUS   = (12104)/2      # [km]
 
@@ -41,22 +41,22 @@ MARS_MASS      = 0.642e+24      # [kg]
 MARS_RADIUS    = (6792)/2       # [km] 
 
 # Jupiter 
-JUPITER_MU     = 1
+JUPITER_MU     = 1.267e+8       # [km^3 / s^2]
 JUPITER_MASS   = 1898e+24       # [kg]
 JUPITER_RADIUS = (142984)/2     # [km]
 
 # Saturn 
-SATURN_MU      = 1
+SATURN_MU      = 3.793e+7       # [km^3 / s^2]
 SATURN_MASS    = 568e+24        # [kg]
 SATURN_RADIUS  = (120536)/2     # [km]
 
 # Uranus 
-URANUS_MU      = 1
+URANUS_MU      = 5.794e+6       # [km^3 / s^2]
 URANUS_MASS    = 86.8e+24       # [kg]
 URANUS_RADIUS  = (51118)/2      # [km]
 
 # Neptune 
-NEPTUNE_MU     = 1
+NEPTUNE_MU     = 6.837e+6       # [km^3 / s^2]
 NEPTUNE_MASS   = 102e+24        # [kg]
 NEPTUNE_RADIUS = (49528)/2      # [km]
 
